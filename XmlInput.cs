82daf76cfe4c861b39ef3cb8d4a265dff146032e
Chris <cidzerda@sbcglobal.net>
2015-11-01 20:17:11 -0800

    Remove the try-catch block from the XML input class
    Have the caller handler exceptions.

--- XmlInput.cs
+++ XmlInput.cs
@@ -10,27 +10,19 @@ namespace Pard
     {
         public Grammar Read(TextReader reader)
         {
-            try
+            var xml = XDocument.Load(reader).Element("grammar");
+            var productions = new List<Production>();
+            foreach(var rule in xml.Element("rules").Elements("rule"))
             {
-                var xml = XDocument.Load(reader).Element("grammar");
-                var productions = new List<Production>();
-                foreach(var rule in xml.Element("rules").Elements("rule"))
-                {
-                    var lhs = (string)rule.Attribute("name");
-                    var rhs = from s in rule.Elements()
-                              where s.Name != "action"
-                              let n = (string)s.Attribute("name") ?? (string)s.Attribute("value")
-                              select s.Name == "nonterminal" ? (Symbol)new Nonterminal(n) : s.Name == "literal" ? new Terminal(n.Substring(0, 1)) : new Terminal(n);
-                    var production = new Production(new Nonterminal(lhs), rhs);
-                    productions.Add(production);
-                }
-                return new Grammar(productions);
+                var lhs = (string)rule.Attribute("name");
+                var rhs = from s in rule.Elements()
+                          where s.Name != "action"
+                          let n = (string)s.Attribute("name") ?? (string)s.Attribute("value")
+                          select s.Name == "nonterminal" ? (Symbol)new Nonterminal(n) : s.Name == "literal" ? new Terminal(n.Substring(0, 1)) : new Terminal(n);
+                var production = new Production(new Nonterminal(lhs), rhs);
+                productions.Add(production);
             }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                return null;
-            }
+            return new Grammar(productions);
         }
     }
 }
