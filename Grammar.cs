7cc36cc020dfd068625dd1c816034a763231535b
Chris <cidzerda@sbcglobal.net>
2015-11-07 10:31:00 -0800

    Move static methods into an inner class

--- Grammar.cs
+++ Grammar.cs
@@ -45,7 +45,7 @@ namespace Pard
             augmentedProductions.AddRange(referencedProductions);
 
             // Algorithm 4.9, p. 231
-            var items = Items(augmentedProductions).Select((s, i) => new { Set = s, Index = i }).ToDictionary(p => p.Set, p => p.Index);
+            var items = Augmented.Items(augmentedProductions).Select((s, i) => new { Set = s, Index = i }).ToDictionary(p => p.Set, p => p.Index);
 
             // Algorithm 4.10, p. 234
             // Create the action table.
@@ -74,185 +74,188 @@ namespace Pard
             gotos = c.SelectMany(e => e).ToList();
         }
 
-        // closure(I), p. 232
-        private static Item.Set Closure(Item.Set items, IReadOnlyList<Production> productions, IEnumerable<Production> expandedProductions)
+        class Augmented
         {
-            int count;
-            do
+            // closure(I), p. 232
+            private static Item.Set Closure(Item.Set items, IReadOnlyList<Production> productions, IEnumerable<Production> expandedProductions)
             {
-                count = items.Count;
-
-                // for each item [A → α∙Bβ, a] in I,
-                // each production B → γ in G',
-                // and each terminal b in FIRST(βa)
-                // such that [B → ∙γ, b] is not in I do
-                var q = from i in items.AsQueryable()
-                        let ip = productions[i.ProductionIndex]
-                        where i.DotPosition < ip.Rhs.Count
-                        let n = ip.Rhs[i.DotPosition] as Nonterminal
-                        where n != null
-                        let r = ip.Rhs.Skip(i.DotPosition + 1)
-                        let l = i.Lookahead
-                        from p in productions.Select((x, y) => new { Production = x, Index = y })
-                        where p.Production.Lhs == n
-                        from b in First(r.Concat(new[] { l }), expandedProductions)
-                        select new Item(p.Index, 0, b);
-
-                // add [B → ∙γ, b] to I;
-                items.UnionWith(q.ToList()); // Use ToList to prevent an iteration exception.
-
-                // until no more items can be added to I;
-            } while(count < items.Count);
-
-            // return I
-            return items;
-        }
-
-        // goto(I, X), p. 232
-        private static Item.Set Goto(Item.Set items, Symbol symbol, IReadOnlyList<Production> productions, IEnumerable<Production> expandedProductions)
-        {
-            // let J be the set of items [A → αX∙β, a] such that
-            // [A → α∙Xβ, a] is in I;
-            var q = from i in items.AsQueryable()
-                    let p = productions[i.ProductionIndex]
-                    let d = i.DotPosition
-                    where d < p.Rhs.Count && p.Rhs[d] == symbol
-                    select new Item(i.ProductionIndex, d + 1, i.Lookahead);
-
-            // return closure(J)
-            return Closure(new Item.Set(q), productions, expandedProductions);
-        }
-
-        // items(G'), p. 232
-        private static List<Item.Set> Items(IReadOnlyList<Production> productions)
-        {
-            // Create a collection of symbols used in the grammar.
-            var symbols = new HashSet<Symbol>(productions.SelectMany(p => p.Rhs));
-
-            // Create a collection of terminals.
-            var terminals = new HashSet<Terminal>(productions.SelectMany(p => p.Rhs.OfType<Terminal>()));
-
-            // Collect the expanded productions.
-            var expandedProductions = CollectExpandedProductions(productions);
-
-            // Create a list to hold the items added to the closure.  Their
-            // indicies will be the state indicies.
-            var items = new List<Item.Set>();
-
-            // C := {closure({[S' → ∙S, $]})};
-            var c = new HashSet<Item.Set>(new[] { Closure(new Item.Set(new[] { new Item(0, 0, Terminal.AugmentedEnd) }), productions, expandedProductions) });
-            items.Add(c.First());
-
-            // repeat
-            int count;
-            do
-            {
-                count = c.Count;
-
-                // for each set of items I in C and each grammar symbol X
-                foreach(var itemSet in c.ToList()) // Use ToList to prevent an iteration exception.
+                int count;
+                do
                 {
-                    foreach(var symbol in symbols)
-                    {
-                        // such that goto(I, X) is not empty and not in C do
-                        var g = Goto(itemSet, symbol, productions, expandedProductions);
-                        if(g.Any() && !c.Contains(g))
-                        {
-                            // add goto(I, X) to C
-                            c.Add(g);
-                            items.Add(g);
-                        }
+                    count = items.Count;
 
-                        if(g.Any())
+                    // for each item [A → α∙Bβ, a] in I,
+                    // each production B → γ in G',
+                    // and each terminal b in FIRST(βa)
+                    // such that [B → ∙γ, b] is not in I do
+                    var q = from i in items.AsQueryable()
+                            let ip = productions[i.ProductionIndex]
+                            where i.DotPosition < ip.Rhs.Count
+                            let n = ip.Rhs[i.DotPosition] as Nonterminal
+                            where n != null
+                            let r = ip.Rhs.Skip(i.DotPosition + 1)
+                            let l = i.Lookahead
+                            from p in productions.Select((x, y) => new { Production = x, Index = y })
+                            where p.Production.Lhs == n
+                            from b in First(r.Concat(new[] { l }), expandedProductions)
+                            select new Item(p.Index, 0, b);
+
+                    // add [B → ∙γ, b] to I;
+                    items.UnionWith(q.ToList()); // Use ToList to prevent an iteration exception.
+
+                    // until no more items can be added to I;
+                } while(count < items.Count);
+
+                // return I
+                return items;
+            }
+
+            // goto(I, X), p. 232
+            private static Item.Set Goto(Item.Set items, Symbol symbol, IReadOnlyList<Production> productions, IEnumerable<Production> expandedProductions)
+            {
+                // let J be the set of items [A → αX∙β, a] such that
+                // [A → α∙Xβ, a] is in I;
+                var q = from i in items.AsQueryable()
+                        let p = productions[i.ProductionIndex]
+                        let d = i.DotPosition
+                        where d < p.Rhs.Count && p.Rhs[d] == symbol
+                        select new Item(i.ProductionIndex, d + 1, i.Lookahead);
+
+                // return closure(J)
+                return Closure(new Item.Set(q), productions, expandedProductions);
+            }
+
+            // items(G'), p. 232
+            public static List<Item.Set> Items(IReadOnlyList<Production> productions)
+            {
+                // Create a collection of symbols used in the grammar.
+                var symbols = new HashSet<Symbol>(productions.SelectMany(p => p.Rhs));
+
+                // Create a collection of terminals.
+                var terminals = new HashSet<Terminal>(productions.SelectMany(p => p.Rhs.OfType<Terminal>()));
+
+                // Collect the expanded productions.
+                var expandedProductions = CollectExpandedProductions(productions);
+
+                // Create a list to hold the items added to the closure.  Their
+                // indicies will be the state indicies.
+                var items = new List<Item.Set>();
+
+                // C := {closure({[S' → ∙S, $]})};
+                var c = new HashSet<Item.Set>(new[] { Closure(new Item.Set(new[] { new Item(0, 0, Terminal.AugmentedEnd) }), productions, expandedProductions) });
+                items.Add(c.First());
+
+                // repeat
+                int count;
+                do
+                {
+                    count = c.Count;
+
+                    // for each set of items I in C and each grammar symbol X
+                    foreach(var itemSet in c.ToList()) // Use ToList to prevent an iteration exception.
+                    {
+                        foreach(var symbol in symbols)
                         {
-                            // Add it as a target state of the source state.
-                            if(!itemSet.Gotos.ContainsKey(symbol))
+                            // such that goto(I, X) is not empty and not in C do
+                            var g = Goto(itemSet, symbol, productions, expandedProductions);
+                            if(g.Any() && !c.Contains(g))
                             {
-                                itemSet.Gotos.Add(symbol, g);
+                                // add goto(I, X) to C
+                                c.Add(g);
+                                items.Add(g);
                             }
-                            else if(itemSet.Gotos[symbol] != g)
+
+                            if(g.Any())
                             {
-                                Console.Error.WriteLine("warning: goto conflict between {0} -> {1} and {0} -> {2} on {3}",
-                                    itemSet, itemSet.Gotos[symbol], g, symbol);
+                                // Add it as a target state of the source state.
+                                if(!itemSet.Gotos.ContainsKey(symbol))
+                                {
+                                    itemSet.Gotos.Add(symbol, g);
+                                }
+                                else if(itemSet.Gotos[symbol] != g)
+                                {
+                                    Console.Error.WriteLine("warning: goto conflict between {0} -> {1} and {0} -> {2} on {3}",
+                                        itemSet, itemSet.Gotos[symbol], g, symbol);
+                                }
                             }
                         }
                     }
-                }
-                // until no more sets of items can be added to C
-            } while(count < c.Count);
+                    // until no more sets of items can be added to C
+                } while(count < c.Count);
 
-            return items;
-        }
+                return items;
+            }
 
-        private static HashSet<Terminal> First(IEnumerable<Symbol> symbols, IEnumerable<Production> expandedProductions)
-        {
-            var first = new HashSet<Terminal>();
-
-            foreach(var symbol in symbols)
+            private static HashSet<Terminal> First(IEnumerable<Symbol> symbols, IEnumerable<Production> expandedProductions)
             {
-                var symbolFirst = First(symbol, expandedProductions);
-                first.UnionWith(symbolFirst);
-                if(!symbolFirst.Contains(Terminal.Epsilon))
+                var first = new HashSet<Terminal>();
+
+                foreach(var symbol in symbols)
                 {
-                    first.Remove(Terminal.Epsilon);
-                    break;
+                    var symbolFirst = First(symbol, expandedProductions);
+                    first.UnionWith(symbolFirst);
+                    if(!symbolFirst.Contains(Terminal.Epsilon))
+                    {
+                        first.Remove(Terminal.Epsilon);
+                        break;
+                    }
                 }
+
+                return first;
             }
 
-            return first;
-        }
-
-        // FIRST(X), p. 189
-        private static HashSet<Terminal> First(Symbol symbol, IEnumerable<Production> expandedProductions)
-        {
-            var first = new HashSet<Terminal>();
-
-            if(symbol is Terminal)
+            // FIRST(X), p. 189
+            private static HashSet<Terminal> First(Symbol symbol, IEnumerable<Production> expandedProductions)
             {
-                first.Add((Terminal)symbol);
-            }
-            else
-            {
-                var q = from p in expandedProductions
-                        where p.Lhs == symbol
-                        select p.Rhs.FirstOrDefault() ?? Terminal.Epsilon;
-                first.UnionWith(q.OfType<Terminal>());
-            }
+                var first = new HashSet<Terminal>();
 
-            return first;
-        }
-
-        // This assists the FIRST(X) procedure by collecting all productions
-        // with additional productions not in the grammar synthesized by
-        // removing initial non-terminals from productions where those initial
-        // non-terminals are the left-hand side of a production that derives ε.
-        private static HashSet<Production> CollectExpandedProductions(IEnumerable<Production> productions)
-        {
-            var expandedProductions = new HashSet<Production>(productions);
-
-            int count;
-            do
-            {
-                count = expandedProductions.Count;
-
-                // Find all ε productions.  Retrieve their left-hand sides.
-                var epsilonLhss = new HashSet<Nonterminal>(expandedProductions.Where(p => !p.Rhs.Any()).Select(p => p.Lhs));
-
-                // For each of those, add to the expanded productions a
-                // production synthesized from each production with that symbol
-                // as its first right-hand side symbol by excluding that symbol
-                // as its first right-hand side symbol.
-                foreach(var epsilonLhs in epsilonLhss)
+                if(symbol is Terminal)
+                {
+                    first.Add((Terminal)symbol);
+                }
+                else
                 {
                     var q = from p in expandedProductions
-                            where p.Rhs.FirstOrDefault() == epsilonLhs
-                            select new Production(p.Lhs, p.Rhs.Skip(1), 0);
-                    expandedProductions.UnionWith(q.ToList()); // Use ToList to prevent an iteration exception.
+                            where p.Lhs == symbol
+                            select p.Rhs.FirstOrDefault() ?? Terminal.Epsilon;
+                    first.UnionWith(q.OfType<Terminal>());
                 }
 
-            } while(count < expandedProductions.Count);
+                return first;
+            }
 
-            return expandedProductions;
+            // This assists the FIRST(X) procedure by collecting all productions
+            // with additional productions not in the grammar synthesized by
+            // removing initial non-terminals from productions where those initial
+            // non-terminals are the left-hand side of a production that derives ε.
+            private static HashSet<Production> CollectExpandedProductions(IEnumerable<Production> productions)
+            {
+                var expandedProductions = new HashSet<Production>(productions);
+
+                int count;
+                do
+                {
+                    count = expandedProductions.Count;
+
+                    // Find all ε productions.  Retrieve their left-hand sides.
+                    var epsilonLhss = new HashSet<Nonterminal>(expandedProductions.Where(p => !p.Rhs.Any()).Select(p => p.Lhs));
+
+                    // For each of those, add to the expanded productions a
+                    // production synthesized from each production with that symbol
+                    // as its first right-hand side symbol by excluding that symbol
+                    // as its first right-hand side symbol.
+                    foreach(var epsilonLhs in epsilonLhss)
+                    {
+                        var q = from p in expandedProductions
+                                where p.Rhs.FirstOrDefault() == epsilonLhs
+                                select new Production(p.Lhs, p.Rhs.Skip(1), 0);
+                        expandedProductions.UnionWith(q.ToList()); // Use ToList to prevent an iteration exception.
+                    }
+
+                } while(count < expandedProductions.Count);
+
+                return expandedProductions;
+            }
         }
 
         public class ActionEntry
