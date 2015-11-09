d66bca394641f4a19ebe3c60b3036e38e3afcefc
Chris <cidzerda@sbcglobal.net>
2015-11-08 21:31:07 -0800

    Use NMAKE to update the YaccParser class

--- Pard/Pre-build.cmd
+++ Pard/Pre-build.cmd
@@ -1,3 +1,4 @@
+REM(); /*
 @ECHO OFF
 SETLOCAL
 
@@ -5,29 +6,35 @@ IF "%~1" == "" (
 	ECHO usage: %~nx0 ConfigurationName
 	EXIT /B 2
 )
+PATH %PATH%;C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin
 CD /D "%~dp0"
-SET PARD="..\Pard\bin\%~1\Pard.exe"
-SET T=%TEMP%\%RANDOM%.cs
 SET OUTPUT=YaccInput.xml.cs
+SET PARD="..\TestResults\Pard.exe"
+SET T=%TEMP%\%RANDOM%.make
+find "goto" %OUTPUT% > nul
+IF ERRORLEVEL 1 DEL /F /Q %OUTPUT%
 IF EXIST %PARD% (
-	%PARD% --namespace=Pard.Test --parser-class-name=YaccParser --scanner-class-name=YaccScanner YaccInput.xml %T%
-	IF NOT EXIST %OUTPUT% (
-		ECHO Creating %OUTPUT%
-		MOVE /Y %T% %OUTPUT%
+	cscript //nologo //E:JScript %0 YaccInput.xml > %T%
+	nmake -nologo "ConfigurationName=%~1" -f %T%
+	IF ERRORLEVEL 1 (
+		DEL /F /Q %T%
 		EXIT /B 1
 	) ELSE (
-		FC %T% %OUTPUT% > nul
-		IF ERRORLEVEL 1 (
-			ECHO Updating %OUTPUT%
-			MOVE /Y %T% %OUTPUT%
-			EXIT /B 1
-		) ELSE (
-			ECHO No change to %OUTPUT%
-			DEL /F /Q %T%
-			EXIT /B 0
-		)
+		DEL /F /Q %T%
+		EXIT /B 0
 	)
 ) ELSE (
 	ECHO XML-only parser
+	ECHO using System; > %OUTPUT%
 	EXIT /B 0
 )
+GOTO :EOF
+*/
+function REM() {
+	var xmlFileName = WScript.Arguments(0);
+	var csFileName = xmlFileName + ".cs";
+	var fout = WScript.StdOut;
+	fout.WriteLine(csFileName + ':');
+	fout.WriteLine('\tIF EXIST ' + csFileName + ' DEL /F /Q ' + csFileName);
+	fout.WriteLine('\t"..\\TestResults\\Pard.exe" --namespace=Pard --parser-class-name=YaccParser --scanner-class-name=YaccScanner ' + xmlFileName + ' ' + csFileName);
+}
