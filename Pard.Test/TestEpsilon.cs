97f8dc23a1745dc8d4641d28defac5b49fa099cf
Chris <cidzerda@sbcglobal.net>
2015-11-14 11:02:58 -0800

    Add the "epsilon" test

---
+++ Pard.Test/TestEpsilon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pard.Test
+{
+    [TestClass]
+    public class TestEpsilon
+    {
+        [TestMethod]
+        public void ParseEmptyTokenStream()
+        {
+            var parser = new ParserEpsilon("");
+            Assert.IsTrue(parser.Parse());
+            Assert.AreEqual("<>", parser.Result.ToString());
+        }
+
+        [TestMethod]
+        public void ParseNonemptyTokenStream()
+        {
+            var parser = new ParserEpsilon("aaa");
+            Assert.IsTrue(parser.Parse());
+            Assert.AreEqual("<><a><a><a>", parser.Result.ToString());
+        }
+    }
+
+    public partial class ParserEpsilon
+    {
+        public readonly StringBuilder Result = new StringBuilder();
+
+        public ParserEpsilon(string tokenStream) : this(new Scanner(tokenStream)) { }
+    }
+}
