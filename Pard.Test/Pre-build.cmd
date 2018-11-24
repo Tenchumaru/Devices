478216e375cea6650089cedcdf4f268da55dc0ad
Chris <github@adrezdi.com>
2018-11-23 21:18:31 -0800

    Correct and use the Pard.Test pre-build command

--- Pard.Test/Pre-build.cmd
+++ Pard.Test/Pre-build.cmd
@@ -2,12 +2,14 @@ REM(); /*
 @ECHO OFF
 SETLOCAL
 
+FOR /F "delims=" %%I IN ('DIR /B /S "C:\Program Files (x86)\Microsoft Visual Studio\nmake.exe"') DO PATH %PATH%;%%~dpI
 CD /D "%~dp0"
-SET T=%TEMP%\%RANDOM%.make
-find ".xml" "%~dp0Pard.Test.csproj" | find "Content" | cscript //nologo //E:JScript %0 > %T%
-"C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\nmake" -nologo "ConfigurationName=%~1" -f %T% all
+SET T=%TEMP%\Pard.Test.%RANDOM%.make
+find ".xml" "%~dp0Pard.Test.csproj" | find "Content" | cscript //nologo //E:JScript "%~nx0" %1 > %T%
+nmake -nologo "ConfigurationName=%~1" -f %T% all
+SET EXIT_CODE=%ERRORLEVEL%
 DEL /F /Q %T%
-GOTO :EOF
+EXIT /B %EXIT_CODE%
 */
 function REM() {
 	var fin = WScript.StdIn, fout = WScript.StdOut;
@@ -16,6 +18,7 @@ function REM() {
 		var s = fin.ReadLine();
 		xmlFileNames.push(s.split('"')[1]);
 	}
+	fout.WriteLine("ConfigurationName=" + WScript.Arguments(0));
 	fout.Write("all:");
 	for(var i = 0, n = xmlFileNames.length; i < n; ++i) {
 		var xmlFileName = xmlFileNames[i];
