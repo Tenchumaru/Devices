fc90ca1285678048c5e7351468e760cce53b715a
Tenchumaru <4085772+Tenchumaru@users.noreply.github.com>
2021-09-21 17:57:44 -0700

    Invoke vcvars64.bat instead of setting path

--- Pard.Test/Pre-build.cmd
+++ Pard.Test/Pre-build.cmd
@@ -2,7 +2,7 @@ REM(); /*
 @ECHO OFF
 SETLOCAL
 
-FOR /F "delims=" %%I IN ('DIR /B /S "C:\Program Files (x86)\Microsoft Visual Studio\nmake.exe"') DO PATH %PATH%;%%~dpI
+FOR /F "delims=" %%I IN ('DIR /B /S "%SystemDrive%\Program Files (x86)\Microsoft Visual Studio\vcvars64.bat"') DO CALL "%%~I"
 CD /D "%~dp0"
 SET T=%TEMP%\Pard.Test.%RANDOM%.make
 find ".xml" "%~dp0Pard.Test.csproj" | find "Content" | cscript //nologo //E:JScript "%~nx0" %1 > %T%
