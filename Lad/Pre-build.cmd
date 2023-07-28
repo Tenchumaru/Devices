a85e51cb1260dcb2e83a178896f5d806817950e0
Tenchumaru <4085772+Tenchumaru@users.noreply.github.com>
2023-07-28 08:28:43 -0700

    Add assembly information

--- Lad/Pre-build.cmd
+++ Lad/Pre-build.cmd
@@ -1,10 +1,12 @@
 @ECHO OFF
 SETLOCAL
 
+IF "%~1" == "" EXIT /B 2
+SET CONFIGURATION=%~1
 CD /D "%~dp0"
 SET O=RegularExpressionParser.g.cs
 IF NOT EXIST obj MD obj
-Pard.exe --namespace=Lad --parser-class-name=RegularExpressionParser --scanner-class-name=RegularExpressionScanner RegularExpressionParser.xml obj\%O%
+"..\Pard\bin\%CONFIGURATION%\net6.0\Pard.exe" --parser-class-declaration="namespace Lad{public partial class RegularExpressionParser" --scanner-class-name=RegularExpressionScanner RegularExpressionParser.xml obj\%O%
 fc obj\%O% %O% > NUL 2> NUL
 IF NOT ERRORLEVEL 1 (
 	ECHO Same
