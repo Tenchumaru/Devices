74bbddc19b4f950453d5fb0d97c9dfb6ce7de863
Tenchumaru <4085772+Tenchumaru@users.noreply.github.com>
2023-11-11 20:48:50 -0800

    Add a test for trailing context

---
+++ Lad.Test/Test3l.cs
@@ -0,0 +1,29 @@
+﻿namespace Lad.Test {
+	[TestClass]
+	public partial class Test3l {
+		private readonly TextReader reader = new StringReader("abbabbaba zxy zxyy zy zyy");
+
+		[TestMethod]
+		public void Doit() {
+			// Assemble
+			var expected = new[] { "ab", "ab", "zx", "zx", "z", "z" };
+
+			// Act
+			string[] actual = Enumerable.Range(0, int.MaxValue).Select(_ => Read()).TakeWhile(t => t != null).Select(t => t!.Value).ToArray();
+
+			// Assert
+			var q = expected.Zip(actual);
+			foreach ((string First, string Second) in q) {
+				Assert.AreEqual(First, Second);
+			}
+		}
+
+		internal class Token {
+			internal string Value;
+
+			internal Token(string value) {
+				Value = value;
+			}
+		}
+	}
+}
